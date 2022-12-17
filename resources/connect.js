e7916883bd8bc6380e8c1298bbccaef76cee15a9
04xavierCarmona <112212887+04xavierCarmona@users.noreply.github.com>
2022-12-16 20:42:20 -0500

    codigo

--- resources/connect.js
+++ resources/connect.js
@@ -6,6 +6,10 @@ var cors = require("cors");
 app.use(express.json());
 app.use(cors());
 
+
+app.use(express.static(__dirname + '/'));
+
+
 var conexion = mysql.createConnection({
     host: "localhost",
     user: "root",
